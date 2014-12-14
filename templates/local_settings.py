318ad6972682c4202a3d14af9071fe9fb3391fd2
TomV <tom@viner.tv>
2014-12-14 02:32:47 +0000

    expand database playbook

---
+++ templates/local_settings.py
@@ -0,0 +1,27 @@
+# Database
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        # sudo apt-get install
+        #    postgresql-9.3
+        #    postgresql-server-dev-9.3
+        #    postgresql-contrib-9.3
+        # For new Databases:
+        # psql -d template1 -c 'create extension hstore;'
+        # For existing: ($ src/manage.py dbshell)
+        # github_hook_collector=# create extension hstore;
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # https://help.ubuntu.com/community/PostgreSQL#Basic_Server_Setup
+        # sudo -u postgres psql postgres
+        # \password postgres
+        'USER': 'projadm',
+        'PASSWORD': 'qwerty',
+        # createdb github_hook_collector
+        'NAME': 'github_hook_collector',
+    }
+}
+
+LOCAL_INSTALLED_APPS = (
+    'django_extensions',
+)
